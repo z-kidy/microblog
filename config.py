4cff326259eb8497e89e94fc86d24c32cf54beee
z-kidy <zjindiss@gmail.com>
2014-10-23 00:56:50 +0800

    class 4

---
+++ config.py
@@ -0,0 +1,18 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'jindi135'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#The SQLALCHEMY_DATABASE_URI is required by the Flask-SQLAlchemy extension. This is the path of our database file.
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#The SQLALCHEMY_MIGRATE_REPO is the folder where we will store the SQLAlchemy-migrate data files.
